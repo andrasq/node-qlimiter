044558dd7aec7d67979abc6566e6fcbdce31b557
Andras <andras@kinvey.com>
2016-12-23 14:40:52 -0500

    test-Limit, test-LimitConcurrent; 0.2.0-pre

---
+++ test-Limit.js
@@ -0,0 +1,49 @@
+/**
+ * qlimiter - nodejs call limiter
+ *
+ * Copyright (C) 2016 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ */
+
+'use strict';
+
+var qlimiter = require('./');
+var Limiter = qlimiter.Limiter;
+var Limit = qlimiter.Limit;
+
+module.exports = {
+    'should export Limit': function(t) {
+        t.equal(typeof Limit, 'function');
+        t.done();
+    },
+
+    'should have expected methods': function(t) {
+        var lim = new Limit();
+        t.equal(typeof lim.acquire, 'function');
+        t.equal(typeof lim.release, 'function');
+        t.equal(typeof lim.setOnUnblock, 'function');
+        t.done();
+    },
+
+    'setOnUnblock should reject non-function': function(t) {
+        var lim = new Limit();
+        t.throws(function(){ lim.setOnUnblock(1) });
+        t.done();
+    },
+
+    'should release without an onUnblock callback': function(t) {
+        var lim = new Limit();
+        t.equal(lim.acquire(), true);
+        lim.release();
+        t.done();
+    },
+
+    'should invoke onUnblock when restriction lifted': function(t) {
+        var lim = new Limit();
+        lim.setOnUnblock(function(){
+            t.done();
+        })
+        t.equal(typeof lim.acquire(), 'boolean');
+        lim.release();
+    },
+};
---
+++ test-LimitConcurrent.js
@@ -0,0 +1,45 @@
+/**
+ * qlimiter - nodejs call limiter
+ *
+ * Copyright (C) 2016 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ */
+
+'use strict';
+
+var qlimiter = require('./');
+var LimitConcurrent = require('./lib/LimitConcurrent');
+
+module.exports = {
+    beforeEach: function(done) {
+        this.maxConcurrent = 2;
+        this.limit = new LimitConcurrent(this.maxConcurrent);
+        done();
+    },
+
+    'should be a Limit': function(t) {
+        t.assert(this.limit instanceof qlimiter.Limit);
+        t.done();
+    },
+
+    'should cap running calls': function(t) {
+        var ok;
+        this.limit.acquire();
+        ok = this.limit.acquire();
+        t.assert(ok);
+        ok = this.limit.acquire();
+        t.assert(!ok);
+        t.equal(this.limit.running, 2);
+        t.done();
+    },
+
+    'should unblock on release': function(t) {
+        var called = false;
+        function onUnblock() { called = true };
+        this.limit.setOnUnblock(onUnblock);
+        this.limit.acquire();
+        this.limit.release();
+        t.assert(called);
+        t.done();
+    },
+};
